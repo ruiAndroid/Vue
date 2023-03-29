298163474c849efb84239cc4f714f5dbeb28f0e0
jianrui <jianrui@fun.tv>
2023-03-29 19:38:35 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,143 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+
+import * as echarts from 'echarts'  
+
+</script>
+
+<script>
+export default {
+  data() {
+    return {
+      centerDialogVisible: false,
+      
+    }
+  },
+  methods: {
+    showDialog() {
+      this.centerDialogVisible=true
+      var that=this
+      
+      setTimeout(() => {
+        var charts = that.$refs.EcharRef
+        var myChart = echarts.init(charts);
+        // 指定图表的配置项和数据
+        var option = {
+            title: {
+              text: 'ECharts 入门示例'
+            },
+            tooltip: {},
+            legend: {
+              data: ['销量']
+            },
+            xAxis: {
+              data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+            },
+            yAxis: {},
+            series: [
+              {
+                name: '销量',
+                type: 'bar',
+                data: [5, 20, 36, 10, 10, 20]
+              }
+            ]
+          };
+          // 使用刚指定的配置项和数据显示图表。
+          myChart.setOption(option);
+      }, 300);
+     
+    }
+  }
+}
+</script>
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <el-button v-on:click="showDialog">fuck</el-button>
+    
+    <el-dialog
+      v-model="centerDialogVisible"
+      title="Warning"
+      width="30%"
+      align-center
+    >
+      <div ref="EcharRef" class="dialog-content" >
+
+      </div>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="centerDialogVisible = false">Cancel</el-button>
+          <el-button type="primary" @click="centerDialogVisible = false">
+            Confirm
+          </el-button>
+        </span>
+      </template>
+    </el-dialog>
+  </header>
+
+
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+.dialog-content{
+  width: 800px;
+  height: 600px;
+
+}
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+
+}
+</style>
